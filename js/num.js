08ac1670843b9041dfa4032a5bfd7067d275178c
zmx0142857 <zmx0142857@qq.com>
2017-10-20 22:52:21 +0800

    edit almost every file: paragraph managing

--- js/num.js
+++ js/num.js
@@ -4,23 +4,30 @@ var X = abbr[0];
 var n = parseInt(abbr.substring(1, abbr.length));
 var i;
 
+function decorate(classname, word) {
+	var tag = document.getElementsByClassName(classname);
+	var i;
+	for (i = 0; i < tag.length; i++) {
+		tag[i].innerHTML = '<b>' + word + abbr + '.' + (i+1) + '</b> ' + tag[i].innerHTML;
+	}
+}
+
 // nav
 //var nav = document.getElementsById('nav');
 //if (X = '#')
 //	X = '%23';
 //nav[0].innerHTML = nav[0].innerHTML.replace(/prev/g, X + (n-1)).replace(/next/g, X + (n+1));
 
-// h2
 var h2 = document.getElementsByTagName('h2');
 for (i = 0; i < h2.length; i++) {
 	h2[i].innerHTML = abbr + '.' + (i+1) + ' ' + h2[i].innerHTML;
 }
 
-// note
-var note = document.getElementsByClassName('note');
-for (i = 0; i < note.length; i++) {
-	note[i].innerHTML = '<b>注' + abbr + '.' + (i+1) + '</b> ' + note[i].innerHTML;
-}
+decorate('theorem', '定理');
+decorate('definition', '定义');
+decorate('lemma', '引理');
+decorate('corollary', '推论');
+decorate('note', '注');
 
 // formula num: overwrite
 var num = document.getElementsByClassName('num');
