672842489baa22bc753d7e49baaaa4f4eb72ee3c
zmx0142857 <zmx0142857@qq.com>
2017-10-31 18:30:32 +0800

    edit js/num.js: add make_nav() updated html documents for auto generated nav bar

--- js/num.js
+++ js/num.js
@@ -1,6 +1,8 @@
+'use strict';
+
 var t = document.title;
 var abbr = t.substring(0, t.indexOf(' '));
-var X = abbr[0];
+var x = abbr.substring(0, abbr.search(/[0-9]/));
 var n = parseInt(abbr.substring(1, abbr.length));
 var i;
 
@@ -42,13 +44,40 @@ function decorate(name, word, style=style_name_num, get_by='class') {
 	}
 }
 
-// nav
-//var nav = document.getElementsById('nav');
-//if (X = '#')
-//	X = '%23';
-//nav[0].innerHTML = nav[0].innerHTML.replace(/prev/g, X + (n-1)).replace(/next/g, X + (n+1));
+/*	<div id="nav">
+		<a href="prev.html" id="prev">&lt;&lt;&lt;</a>
+		<a href="content.html" target="_blank">· · ·</a>
+		<a href="next.html" id="next">&gt;&gt;&gt;</a>
+	</div>
+*/
+function make_nav() {
+	var body = document.getElementsByTagName('body');
+	var h1 = document.getElementsByTagName('h1');
+	var nav = document.createElement('div');
+	nav.id = 'nav';
+	body[0].insertBefore(nav, h1[0]);
+	if (n > 1) {
+		var prev = document.createElement('a');
+		prev.href = (x === '#' ? '%23' : x) + (n-1) + '.html';
+		prev.id = 'prev';
+		prev.innerHTML = '&lt;&lt;&lt;';
+		nav.appendChild(prev);
+	}
+	var content = document.createElement('a');
+	content.href = 'content.html';
+	content.target = '_blank';
+	content.innerHTML = '· · ·';
+	nav.appendChild(content);
 
-decorate('h2', '', style_name_num, 'tag')
+	var next = document.createElement('a');
+	next.href = (x === '#' ? '%23' : x) + (n+1) + '.html';
+	next.id = 'next';
+	next.innerHTML = '&gt;&gt;&gt;';
+	nav.appendChild(next);
+}
+
+make_nav();
+decorate('h2', '', style_name_num, 'tag');
 decorate('theorem', '定理');
 decorate('definition', '定义');
 decorate('lemma', '引理');
@@ -59,3 +88,4 @@ decorate('method', '法', style_num);// place this before '证' and '解'
 decorate('prove', '证', style_void);
 decorate('solve', '解', style_void);
 decorate('num', '', style_formula);
+
